cb7f352b0fb35dad961f20067b7ab6ef9b465a36
michael.cunha <mike@MikeBook-Air.local>
2017-12-17 15:35:44 -0500

    Implement Confidence Sequence Method
    Implementation of an early stopping method applicable to monte carlo
    techniques with bounded resampling risk as detailed in
    https://arxiv.org/abs/1611.01675
    This will be used to conduct intractable permutation tests.

--- tbias/utils.py
+++ tbias/utils.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.special import comb
 
 
 def plot_test(frame, title):
@@ -24,3 +25,72 @@ def plot_test(frame, title):
     for i, txt in enumerate(frame.embedding):
         splot.ax.annotate('  '+txt, (frame.p_val.iat[i],
                           frame.effect_size.iat[i]))
+
+
+def n_choose_k(n, k):
+    # uses gamma function and log space to avoid overflow
+    return comb(n, k, exact=False, repetition=False)
+
+
+def calc_criteria(n,α,S):
+    """
+    The main part of the CSM check for early stopping.
+    """
+
+    very_large_num = n_choose_k(n,S)
+    very_small_num = (α**S)*((1-α)**(n-S))
+
+    # avoid underflow
+    if very_small_num == 0:
+        b = 0
+    else:
+        b = very_large_num * very_small_num
+
+    return (n+1) * b
+
+
+def CSM(gen_obs, α=0.05, max_n=10000, ε=0.001):
+    """
+    Confidence Sequence Method
+    As described in https://arxiv.org/abs/1611.01675
+
+    Inputs
+    ======
+    gen_obs: a function that will return a binary value as 
+      part of a sequence. 0 meaning an observed test
+      statistic did not meet or exceed the original test
+      statistic and 1 meaning an obversed test statistic
+      did.
+    α: boundary of the significance level you are trying to
+      determine which side the estimated p-value falls on.
+    max_n: maximum number of observed test statistics to 
+      collect
+    ε: limit on the re-sampling risk, values from the
+      paper were in the interval [0.01,0.0001]
+    """
+
+    s = 0  # num of times observed >= test stat
+
+    for n in range(1,max_n+1):
+
+        if gen_obs() == 1:
+            s += 1
+
+        criteria = calc_criteria(n, α, s)
+
+        if criteria <= ε:
+            print('out of boundary at n =', n)
+            print('criteria: ', str(float(criteria)))
+            print('p-value =', s/n)
+            if 0 >= criteria and criteria <= α:
+                print('reject the null')
+            elif α > criteria and criteria <= 1:
+                print('fail to reject the null')
+            else:
+                print('hmmm something not right: ', float(criteria))
+            break
+
+        if n == 10000:
+            print('Iterations exhausted, p-val =', s/n)
+
+    return n, s
